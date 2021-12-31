f31e9254114d479e1066eba0809189e9ff0392b2
Tdpro1612 <hangthieulong508@gmail.com>
2021-12-31 07:40:39 +0700

    "update test action"
    Signed-off-by: Tdpro1612 <hangthieulong508@gmail.com>

--- action.py
+++ action.py
@@ -3,7 +3,7 @@ from source.save_data import *
 from source.custom_data import *
 
 import yaml
-with open("config/config.yaml", 'r') as stream:
+with open("config/new.yaml", 'r') as stream:
     try:
         data = yaml.safe_load(stream)
     except yaml.YAMLError as error:
@@ -14,19 +14,62 @@ if 'extension' in data.get('HARD','-1'):
 else:
     a=Extensions()
 
-b=create_broswer('chromdrive/chromedriver',a)   
+b=create_browser('chromdrive/chromedriver',a)   
 if 'url' in data.get('HARD','-1'):
     url = data['HARD']['url']
 
 c=Open_web(b,url)
+number=int(data['LOOP'][0].get('number',None))
+start=int(data['LOOP'][1].get('start',None))
+num=start
+data_save=[]
+if number is None:
+    print("bạn vui lòng nhập số lượng cần loop")
+else:
+    if start is None:
+        print("bạn vui lòng nhập số bắt đầu")
+    else:
+        for i in range(start,number+start):
+            action_dict={}
+            long=len(data['LOOP'])
+            if long != 3:
+                print('long',long)
+            else:
+                list_key=data['LOOP'][2].keys()
+                for i in list_key:
+                    if 'xpath' in i:
+                        list_index = i.split('_')
+                        action = list_index[1]
+                        if action is 'click':
+                            xpath=data['LOOP'][2].get(i,None)
+                            if xpath != None:
+                                if 'start' in xpath:
+                                    track_xpath = xpath.split("start")
+                                    path=track_xpath[0]+str(num)+track_xpath[1]
+                                    Click_button(path)
+                        else:
+                            xpath=data['LOOP'][2].get(i,None)
+                            if xpath is not None:
+                                if 'start' in xpath:
+                                    track_xpath = xpath.split('start')
+                                    path=track_xpath[0]+str(num)+track_xpath[1]   
+                                    variable=Find_xpath(b,path)
+                                    type=data['LOOP'][2].get(i+1,None)
+                                    if 'type' in type:
+                                        val=get_data(variable,type=type) 
+                                    action_dict[action]=val  
+                                else:
+                                    path=xpath
+                                    variable=Find_xpath(b,path)
+                                    if 'type' in type:
+                                        val=get_data(variable,type=type)
+                                    action_dict[action]=val             
+            num+=1
+            print(action_dict)
 
-# list_key=data['LOOP']
-# print(list_key)
-for i in data['LOOP'].keys():
-    print(i)
-# number=int(data['LOOP'][0].get('number'))
-# start=int(data['LOOP'][1].get('start'))
-# for i in range(start,number+start):
+# i = data['SAVE'].get('type',None)
+# if i == None:
+# df = pd.DataFrame(data,columns = [])  
     
     
     
